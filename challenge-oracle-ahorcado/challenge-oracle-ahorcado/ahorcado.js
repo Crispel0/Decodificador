cc245fc80ab47180337a82c8236596a1c23fedc8
cristian <penagoscristian61@gmail.com>
2022-02-11 11:22:25 -0500

    agregando juego ahorcado

--- challenge-oracle-ahorcado/challenge-oracle-ahorcado/ahorcado.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#bton")
-
-bton.addevenlistener("dblclick"){
-    console.log("Doble Click")
-}
-
-var diccionario=['estudio', 'programacion', 'videojuegos', 'palabra', 'lenguajes', 'observar']
